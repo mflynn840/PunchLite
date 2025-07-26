a82649ae23df574b286c5af444d69a0624be5c12
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-07-26 05:29:57 -0400

    create a springboot project with maven

---
+++ PunchLiteDemo/src/main/java/michael/PunchLiteDemo/PunchLiteDemoApplication.java
@@ -0,0 +1,13 @@
+package michael.PunchLiteDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchLiteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchLiteDemoApplication.class, args);
+	}
+
+}
---
+++ PunchLiteDemo/src/test/java/michael/PunchLiteDemo/PunchLiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package michael.PunchLiteDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchLiteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
